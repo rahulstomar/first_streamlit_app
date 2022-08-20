1cf4e67f1671dadc47f77db627ebfe158fb3faa0
Rahul Singh Tomar <rahultomar89@yahoo.com>
2022-08-20 16:57:07 +0530

    Update streamlit_app.py
    We have an issue with control flow as on any action our insert statement was executed.

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output json normalized data as table
 streamlit.dataframe(fruityvice_normalized)
 
+# don't run anything past here while we troubleshoot
+streamlit.stop();
+
 # snowflake python connector
 import snowflake.connector
 
