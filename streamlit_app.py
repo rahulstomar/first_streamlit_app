0fbe0d2b575126550d741ac580bfc57397698a78
Rahul Singh Tomar <rahultomar89@yahoo.com>
2022-08-20 17:05:20 +0530

    Update streamlit_app.py
    Added try-catch block with If-Else for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,20 +31,20 @@ streamlit.dataframe(fruits_to_show)
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-#import requests
-BASE_URL = "https://fruityvice.com/api/fruit/"
-# Get input from user for fruit
-fruit_choice = streamlit.text_input("What fruit would you like the information about?",'kiwi')
-streamlit.write('Your choice is ', fruit_choice)
+try:
+  BASE_URL = "https://fruityvice.com/api/fruit/"
+  # Get input from user for fruit
+  fruit_choice = streamlit.text_input("What fruit would you like the information about?",'kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information')
+  else:
+    streamlit.write('Your choice is ', fruit_choice)
+    fruityvice_response = requests.get(BASE_URL + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get(BASE_URL + fruit_choice)
-# streamlit.text(fruityvice_response.json())  # shows only json data
-
-# normalize json data using python
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# output json normalized data as table
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop();
